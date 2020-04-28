fb655557fe76e857b7940bd9767eb1f59e8c948d
acoville@github.com <adam.coville@gmail.com>
2020-04-28 11:37:47 +0930

    VPadController has some basic timing logic set up to either reset or concatenate the input string. I have not been able to test this yet.

--- Valkyrie.App/Valkyrie.Controls/VPadController.cs
+++ Valkyrie.App/Valkyrie.Controls/VPadController.cs
@@ -17,9 +17,56 @@ namespace Valkyrie.Controls
 {
     class VPadController : IController
     {
+        internal DateTime t1 = DateTime.Now;
+        internal DateTime t2;
+        internal TimeSpan timeSinceLastInput = TimeSpan.FromSeconds(1.0);
 
+        // we will go with a reset timer of 1/4 second and see how that feels
 
-        //==================================================================
+        internal TimeSpan resetTime = TimeSpan.FromMilliseconds(250);
+
+        internal string input_ = "";
+        
+        //=======================================================================
+
+        /*---------------------------------------
+         * 
+         * 
+         * 
+         * -------------------------------------*/
+
+        public string Input
+        {
+            set
+            {
+                // how much time has elapsed since the last input?
+
+                t2 = DateTime.Now;
+                timeSinceLastInput = t2 - t1;
+
+                //-- too much time has elapsed since the last input, reset
+                // the string
+
+                if (timeSinceLastInput >= resetTime)
+                {
+                    timeSinceLastInput = TimeSpan.FromSeconds(0.0);
+                    t1 = DateTime.Now;
+
+                    input_ = value;
+                }
+
+                //-- we are still within the window where the string can be 
+                // modified
+
+                else
+                {
+                    input_ += value;
+                    timeSinceLastInput = TimeSpan.FromSeconds(0.0);
+                }
+            }
+        }
+
+        //======================================================================
         public Command SendCommand()
         {
             throw new NotImplementedException();
